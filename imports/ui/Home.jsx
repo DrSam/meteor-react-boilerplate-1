4332d3399eac38a78d69d6105bc42417290c1979
Anna <a.yeranosyan05@gmail.com>
2017-12-06 17:30:36 +0400

    today changes

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -12,49 +12,51 @@ class Home extends React.Component {
 			<div className="pageWrapper" style = {style.page}>
 				<div className="header">
                     <header>
-                        <nav>
-                            <div className="nav-wrapper">
-                                <a href="#" className="brand-logo">
-                                    <img src="https://www.skillshape.com/images/logo-location.png" alt=""/>
-                                </a>
-                                <a href="#" data-activates="mobile-demo" className="button-collapse">
-                                    <i className="mdi mdi-menu"></i>
-                                </a>
-                                <ul className="right hide-on-med-and-down">
-                                    <li><a href=""><i className="mdi mdi-home"></i>Home</a></li>
-                                    <li><a href=""><i className="mdi mdi-account-plus"></i>Student signup</a></li>
-                                    <li><a href=""><i className="mdi mdi-school"></i>Register a school</a></li>
-                                    <li><a href=""><i className="mdi mdi-checkbox-marked-outline"></i>Claim a school</a></li>
-                                    <li><a href=""><i className="mdi mdi-email"></i>Contact us</a></li>
-                                    <li><a href=""><i className="mdi mdi-information"></i>About us</a></li>
-                                    <li><a href=""><i className="mdi mdi-fingerprint"></i>Login</a></li>
-                                </ul>
-                                <ul className="side-nav" id="mobile-demo">
-                                    <li><a href=""><i className="mdi mdi-home"></i>Home</a></li>
-                                    <li><a href=""><i className="mdi mdi-account-plus"></i>Student signup</a></li>
-                                    <li><a href=""><i className="mdi mdi-school"></i>Register a school</a></li>
-                                    <li><a href=""><i className="mdi mdi-checkbox-marked-outline"></i>Claim a school</a></li>
-                                    <li><a href=""><i className="mdi mdi-email"></i>Contact us</a></li>
-                                    <li><a href=""><i className="mdi mdi-information"></i>About us</a></li>
-                                    <li><a href=""><i className="mdi mdi-fingerprint"></i>Login</a></li>
-                                </ul>
-                            </div>
-                        </nav>
+                        <div className="navbar-fixed">
+                            <nav>
+                                <div className="nav-wrapper">
+                                    <a href="#" className="brand-logo">
+                                        <img src="https://www.skillshape.com/images/logo-location.png" alt=""/>
+                                    </a>
+                                    <a href="#" data-activates="mobile-demo" className="button-collapse">
+                                        <i className="mdi mdi-menu"></i>
+                                    </a>
+                                    <ul className="right hide-on-med-and-down">
+                                        <li><a href=""><i className="mdi mdi-home"></i>Home</a></li>
+                                        <li><a href=""><i className="mdi mdi-account-plus"></i>Student signup</a></li>
+                                        <li><a href=""><i className="mdi mdi-school"></i>Register a school</a></li>
+                                        <li><a href=""><i className="mdi mdi-checkbox-marked-outline"></i>Claim a school</a></li>
+                                        <li><a href=""><i className="mdi mdi-email"></i>Contact us</a></li>
+                                        <li><a href=""><i className="mdi mdi-information"></i>About us</a></li>
+                                        <li><a href=""><i className="mdi mdi-fingerprint"></i>Login</a></li>
+                                    </ul>
+                                    <ul className="side-nav" id="mobile-demo">
+                                        <li><a href=""><i className="mdi mdi-home"></i>Home</a></li>
+                                        <li><a href=""><i className="mdi mdi-account-plus"></i>Student signup</a></li>
+                                        <li><a href=""><i className="mdi mdi-school"></i>Register a school</a></li>
+                                        <li><a href=""><i className="mdi mdi-checkbox-marked-outline"></i>Claim a school</a></li>
+                                        <li><a href=""><i className="mdi mdi-email"></i>Contact us</a></li>
+                                        <li><a href=""><i className="mdi mdi-information"></i>About us</a></li>
+                                        <li><a href=""><i className="mdi mdi-fingerprint"></i>Login</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
                     </header>
 				</div>
                 <div className="formWrapper">
                     <div className="row">
                         <form className="col s12">
                             <div className="row">
-                                <div className="input-field col l2">
+                                <div className="input-field col l3 m6 s12">
                                     <input id="icon_prefix" type="text" className="validate" />
                                     <label>Location</label>
                                 </div>
-                                <div className="input-field col l2">
+                                <div className="input-field col l3 m6 s12">
                                     <input id="icon_school" type="tel" className="validate" />
                                     <label>School</label>
                                 </div>
-                                <div className="input-field col l2">
+                                <div className="input-field col l3 m6 s12">
                                     <select>
                                         <option value="" disabled>Choose your option</option>
                                         <option value="1">Option 1</option>
@@ -63,7 +65,7 @@ class Home extends React.Component {
                                     </select>
                                     <label>Materialize Select</label>
                                 </div>
-                                <div className="input-field col l3">
+                                <div className="input-field col l3 m6 s12">
                                     <div className="range-slider-container">
                                         <div className="range-slider" data-step="1"></div>
                                         <div className="range-slider-value clearfix">
@@ -73,80 +75,83 @@ class Home extends React.Component {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="input-field col l3">
-                                    <div className="range-slider-container">
-                                        <div className="range-slider" data-step="3"></div>
-                                        <div className="range-slider-value clearfix">
-                                            <span>Price: &ensp;</span>
-                                            <span id="min1" className="min"></span>
-                                            <span id="max1" className="max"></span>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                         </form>
                     </div>
                 </div>
 				<div className="contentWrapper">
 					<div className="row">
-                        <div className="col l3">
-                            <div className="card card_item">
-                                <div className="card-image item_image">
-                                    <img src="http://5esd.com/wp-content/uploads/2015/08/Banner3.jpg" alt=""/>
+                        <div className="col s12 m6 l3">
+                            <div className="card">
+                                <div className="card-image">
+                                    <img src="http://materializecss.com/images/sample-1.jpg" />
+                                        <span className="card-title">Card Title</span>
                                 </div>
                                 <div className="card-content">
-                                    <span className="card-title">Kenyon's Soo Bahk Do</span>
-                                    <p>Starting $15/Month</p>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                        I am convenient because I require little markup to use effectively.</p>
                                 </div>
                                 <div className="card-action">
-                                    <a href="" className="waves-effect waves-light btn">Join class</a>
+                                    <a href="#">This is a link</a>
                                 </div>
                             </div>
                         </div>
-                        <div className="col l3">
-                            <div className="card card_item">
-                                <div className="card-image item_image">
-                                    <img src="http://sdcombatacademy.com/wp-content/uploads/sites/12/2015/04/xsdca-geo.jpg.pagespeed.ic.6z0kmD1J39.webp" alt=""/>
+                        <div className="col s12 m6 l3">
+                            <div className="card">
+                                <div className="card-image">
+                                    <img src="http://materializecss.com/images/office.jpg" />
+                                    <span className="card-title">Card Title</span>
                                 </div>
                                 <div className="card-content">
-                                    <span className="card-title">San Diego Combat Academy</span>
-                                    <p>Starting $10/Month</p>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                        I am convenient because I require little markup to use effectively.</p>
                                 </div>
                                 <div className="card-action">
-                                    <a href="" className="waves-effect waves-light btn">Join class</a>
+                                    <a href="#">This is a link</a>
                                 </div>
                             </div>
                         </div>
-                        <div className="col l3">
-                            <div className="card card_item">
-                                <div className="card-image item_image">
-                                    <img src="https://www.skillshape.com/images/SkillShape-Whitesmoke.png" alt=""/>
+                        <div className="col s12 m6 l3">
+                            <div className="card">
+                                <div className="card-image">
+                                    <img src="http://materializecss.com/images/sample-1.jpg" />
+                                    <span className="card-title">Card Title</span>
                                 </div>
                                 <div className="card-content">
-                                    <span className="card-title">Manna's Martial Arts</span>
-                                    <p>Starting $12/Month</p>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                        I am convenient because I require little markup to use effectively.</p>
                                 </div>
                                 <div className="card-action">
-                                    <a href="" className="waves-effect waves-light btn">Join class</a>
+                                    <a href="#">This is a link</a>
                                 </div>
                             </div>
                         </div>
-                        <div className="col l3">
-                            <div className="card card_item">
-                                <div className="card-image item_image">
-                                    <img src="http://impactkravmaga.com/wp-content/uploads/2015/04/san-diego-krav-maga-class1.jpg" alt=""/>
+                        <div className="col s12 m6 l3">
+                            <div className="card">
+                                <div className="card-image">
+                                    <img src="http://materializecss.com/images/office.jpg" />
+                                    <span className="card-title">Card Title</span>
                                 </div>
                                 <div className="card-content">
-                                    <span className="card-title">Yoga at Bird Rock Yoga</span>
-                                    <p>Starting $135/Month</p>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                        I am convenient because I require little markup to use effectively.</p>
                                 </div>
                                 <div className="card-action">
-                                    <a href="" className="waves-effect waves-light btn">Join class</a>
+                                    <a href="#">This is a link</a>
                                 </div>
                             </div>
                         </div>
 					</div>
 				</div>
+                <footer className="page-footer">
+                    <div className="footer-copyright">
+                        <div className="container">
+                            <p className="center-align">
+                                © 2017 Copyright Text
+                            </p>
+                        </div>
+                    </div>
+                </footer>
 			</div>
 		);
 	}
